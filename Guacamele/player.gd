54eff153c4182b6fc95836f58c5586b6ae25897f
ArielFigu <santiagoarielf27@gmail.com>
2026-04-14 00:55:56 -0300

    Se agregaron los ataques y animaciones
    se agrego la función cambiar animación para gestionar la animación de cada acción , y se paraliza la velocidad de ciertos golpes o en el caso de punch up  desliza. aun tirar error al finalizar animaciones por que  no hay un retorno al idle o flujo de animaciones o acciones , que si tienen los saltos

--- Guacamele/player.gd
+++ Guacamele/player.gd
@@ -14,8 +14,41 @@ var atacando : = false
 var  esta_congelado : = false 
 var en_pared := false
 var wall_jump := false
-
 func _physics_process(delta: float) -> void:
+	detectar_pared()
+
+	actualizar_direccion()
+	aplicar_gravedad(delta)
+	saltar()
+	atacar()
+	cabezazo()
+	mover()
+
+	limitar_velocidad()
+
+	move_and_slide()
+
+	actualizar_animacion()
+
+
+
+func actualizar_direccion():
+	if atacando or wall_jump:
+		return
+
+	if en_pared:
+		var normal := get_wall_normal()
+		if normal.x != 0:
+			$AnimatedSprite2D.flip_h = normal.x > 0 
+		#comparo la normal para a donde apunta al igual que cuando dalta si esta apunta para la derecha normal >0 flipeo spite que por default mira izq
+		return
+
+	var direction := Input.get_axis("Left", "right")
+	if direction != 0:
+		$AnimatedSprite2D.flip_h = direction < 0 
+		# ya que el spritte mira a la derecha , me aseguro que si la direc da -1 flipeo izq
+
+func aplicar_gravedad(delta):
 	if esta_congelado:
 		return
 
@@ -23,36 +56,44 @@ func _physics_process(delta: float) -> void:
 		var gravedad = get_gravity().y
 
 		if en_pared:
-			var input_dir := Input.get_axis("move_left", "move_right")
+			var input_direccion := Input.get_axis("Left", "right")
 			var normal := get_wall_normal()
 
-			if input_dir != 0 and sign(input_dir) == -sign(normal.x): 
-				# si la normal de la superficie no coinciden o en su defectp con el -(normal) coinciden se queda pegado
+			if input_direccion != 0 and sign(input_direccion) == -sign(normal.x):
+			# si la normal de la superficie no coinciden o en su defectp con el -(normal) coinciden se queda pegado
 				velocity.y = 0
 				return
 			else:
-				gravedad *= 0.3 #gravedad deñ desliz
+				gravedad *= 0.3#gravedad deñ desliz
 
 		elif velocity.y < 0:
-			gravedad *= 0.6 #gravedad de saltar
+			gravedad *= 0.6#gravedad de saltar
 		else:
 			gravedad *= 1.3
 
 		velocity.y += gravedad * delta
 
-	#detectar_pared()
-	saltar()
-	#atacar()
-	#cabezazo()
-	mover()
-	#limitar_velocidad ()
-	move_and_slide()
-	#cambiar_animacion()
+func detectar_pared():
+	en_pared = not is_on_floor() and is_on_wall()
+
+func saltar():
+	if atacando:
+		return
+
+	if Input.is_action_just_pressed("Jump"):
+		if en_pared:
+			saltar_en_pared()
+			return
+
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			cambiar_animacion("Jump")
+
 func saltar_en_pared():
 	if not en_pared:
 		return
 
-	var direccion := Input.get_axis("move_left", "move_right")
+	var direccion := Input.get_axis("Left", "right")
 	if direccion == 0:
 		return
 
@@ -63,28 +104,80 @@ func saltar_en_pared():
 		velocity.y = JUMP_VELOCITY
 		velocity.x = normal.x * walk_Speed * 1.8
 
+		$AnimatedSprite2D.flip_h = velocity.x > 0
+
+		cambiar_animacion("wallJump")
+
+func atacar():
+	if Input.is_action_just_pressed("Puño") and Input.is_action_pressed("up") and not atacando:
+		atacando = true
+		esta_congelado = false
+		
+		velocity.y = -490
+		
+		var direccion = -1 if $AnimatedSprite2D.flip_h else 1
+		velocity.x = direccion * 200
+		
+		cambiar_animacion("punchUP")
+
+func cabezazo():
+	if Input.is_action_just_pressed("Puño") and (Input.is_action_pressed("Left") or Input.is_action_pressed("right")) and not atacando:
+		atacando = true
+		esta_congelado = true
+		velocity = Vector2.ZERO
+		
+		cambiar_animacion("headHit")
+
 func mover():
 	var direccion := Input.get_axis("Left", "right")
+
 	if atacando:
 		return
-	if en_pared:
-		velocity.x= 0
+
+	if en_pared and not wall_jump:
+		velocity.x = 0
 		return
+
 	if wall_jump:
 		return
+
 	if direccion != 0:
-		velocity.x = direccion* walk_Speed  # anotacion pa mi sin la direccion no rota
+		velocity.x = direccion * walk_Speed  # anotacion pa mi sin la direccion no rota
 	else:
-		velocity.x = 0 
-func saltar():
+		velocity.x = 0
+
+func limitar_velocidad():
+	velocity.y = clamp(velocity.y, -600, 900)
+
+
+
+func actualizar_animacion():
 	if atacando:
 		return
-	
-	if Input.is_action_just_pressed("Jump"):
-		if en_pared:
-			saltar_en_pared()
-			return
 
-	
-		if is_on_floor():
-			velocity.y = JUMP_VELOCITY
+	if is_on_floor() or velocity.y > 10:
+		wall_jump = false
+
+	if wall_jump:
+		cambiar_animacion("wallJump")
+		return
+
+	if en_pared:
+		cambiar_animacion("Pared")
+		return
+
+	if not is_on_floor():
+		if velocity.y < 0:
+			cambiar_animacion("Jump")
+		else:
+			cambiar_animacion("fall")
+		return
+
+	if abs(velocity.x) > 1:
+		cambiar_animacion("walk")
+	else:
+		cambiar_animacion("idle")
+
+func cambiar_animacion(nombre):
+	if $AnimatedSprite2D.animation != nombre:
+		$AnimatedSprite2D.play(nombre)
